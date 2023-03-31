d54ecef4e8d5de112988cd0876c876aca3c24d72
Nyaradzo Chinyati <88329176+Nyaradzo20@users.noreply.github.com>
2023-03-31 23:00:36 +0100

    Update 8-getBudgetCurrentYear.js

--- 0x00-ES6_basic/8-getBudgetCurrentYear.js
+++ 0x00-ES6_basic/8-getBudgetCurrentYear.js
@@ -1,5 +1,4 @@
 function getCurrentYear() {
-<<<<<<< HEAD
   const date = new Date();
   return date.getFullYear();
 }
@@ -10,20 +9,6 @@ export default function getBudgetForCurrentYear(income, gdp, capita) {
     [`gdp-${getCurrentYear()}`]: gdp,
     [`capita-${getCurrentYear()}`]: capita,
   };
+
   return budget;
 }
-=======
-    const date = new Date();
-    return date.getFullYear();
-  }
-  
-  export default function getBudgetForCurrentYear(income, gdp, capita) {
-    const budget = {
-      [`income-${getCurrentYear()}`]: income,
-      [`gdp-${getCurrentYear()}`]: gdp,
-      [`capita-${getCurrentYear()}`]: capita,
-    };
-  
-    return budget;
-  }
->>>>>>> 489b7fbf7bcebca55c1f8c3940a3f25fd1a2e049
